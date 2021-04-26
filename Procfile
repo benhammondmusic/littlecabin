e3a48832ca3a15c2bfbc3413e6c389184c385657
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-04-26 11:13:05 -0600

    adds files for heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn littlecabin_project.wsgi
--- littlecabin_project/settings.py
+++ littlecabin_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals())
