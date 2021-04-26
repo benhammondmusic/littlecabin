3ff8b95c1b557801e65a69f31afc067ad03a4634
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-04-26 12:59:21 -0600

    config var for debug

--- littlecabin_project/settings.py
+++ littlecabin_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 # DEBUG = False
-DEBUG = os.environ.get('DEBUG' ,False)
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['herokuapp.com','.localhost', '127.0.0.1', '[::1]']
 
