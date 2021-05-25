b8268548380beebbb9584f554ffc4e23917c55bd
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-05-25 16:26:37 -0600

    adds tests for fetch_weather

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .fetch_weather import update_current_weather
 
-# Create your tests here.
+class WeatherTestCase(TestCase):
+    def test_weather_response(self):
+        """ update_current_weather() method should return
+        - an object with key "temp" and int value, and key "conditions" with string value
+        - temp should be in a valid range for earth degrees F(greater than -50 and less than 150)
+         """
+        current_weather_obj = update_current_weather()
+
+        self.assertEqual(type(current_weather_obj["temp"]), int)
+        self.assertEqual(type(current_weather_obj["conditions"]), str)
+        self.assertGreater(current_weather_obj["temp"], -50)
+        self.assertLess(current_weather_obj["temp"], 150)
+
+
+
+
+#  
